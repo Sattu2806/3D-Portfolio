19bcb2eeccf43e432c431b196e1e37925379f21b
satender maurya <satendermaurya@satenders-MacBook-Air.local>
2023-05-19 23:54:08 +0530

    eight

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,50 @@
-import './App.css'
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from "./components/Navbar/Navbar"
-import Hero from './components/Hero/Hero'
-import { Suspense } from 'react'
-import About from "./components/About/About"
-import Projects from './components/Projects/Projects'
-import Technologies from './components/Technologies/Technologies'
-import Poster from './components/Poster'
-import Testimonials from './components/Testimonials/Testimonials'
-import Contact from './components/Contact/Contact'
+import React, { useState, useEffect, Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Technologies from './components/Technologies/Technologies';
+import Poster from './components/Poster';
+import Testimonials from './components/Testimonials/Testimonials';
+import Contact from './components/Contact/Contact';
+import ProjectMobile from './components/Projects/ProjectMobile';
+
 function App() {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <BrowserRouter>
-        <Navbar/>
+      <Navbar />
+      <Suspense fallback={null}>
+        <Hero />
+      </Suspense>
+      <About />
+      {isDesktop ? (
         <Suspense fallback={null}>
-          <Hero/>
+          <Projects />
         </Suspense>
-
-      <About/>
-      <Projects/>
-      <Technologies/>
-      <Poster/>
-      <Testimonials/>
-      <Contact/>
+      ):(
+          <ProjectMobile/>
+      )}
+      <Technologies />
+      <Poster />
+      <Testimonials />
+      <Contact />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Projects/ProjectMobile.jsx
+++ src/components/Projects/ProjectMobile.jsx
@@ -1,22 +1,7 @@
 import React from "react";
-import { motion } from "framer-motion";
-import { useSpring, animated } from "@react-spring/web";
+import { useSpring} from "@react-spring/web";
 
-const trans = (x, y, s) =>
-  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s}) translateZ(${s}px)`;
-
-const calc = (x, y) => {
-  const BUFFER = 50;
-
-  const why = (y - window.innerHeight / 2) / BUFFER;
-  const ex = (x - window.innerWidth / 2) / BUFFER;
-
-  console.log("why", why);
-  console.log("y", y);
-  return [y / 50, x / 50, 1.1];
-};
-
-const Projects = () => {
+const ProjectMobile = () => {
   const imageCount = 9;
   const springConfig = { mass: 5, tension: 350, friction: 40 };
   const imageProps = Array.from({ length: imageCount }, () => {
@@ -27,20 +12,6 @@ const Projects = () => {
     return { props, set };
   });
 
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.3,
-      },
-    },
-  };
-
-  const item = {
-    hidden: { opacity: 0, scale: 0 },
-    show: { opacity: 1, scale: 1, transition: { type: "spring" } },
-  };
 
   // Define an array of links corresponding to each image
   const imageLinks = [
@@ -56,7 +27,7 @@ const Projects = () => {
   ];
 
   return (
-    <div id="projects" className="w-11/12 md:w-10/12 mx-auto mt-32 mb-40">
+    <div id="projects" className="w-11/12 mx-auto mt-32 mb-40">
       <div className="text-center mb-20">
         <span className="border-b-2 text-4xl pink-text-gradient border-pink-600">
           Projects
@@ -66,17 +37,12 @@ const Projects = () => {
         </div>
       </div>
       <div className="w-11/12 md:w-11/12 mx-auto">
-        <motion.div
-          variants={container}
-          viewport={{ once: true, amount: 0.5 }}
-          initial="hidden"
-          whileInView="show"
+        <div
           className="grid mx-auto gap-2 xl:grid-cols-3 md:grid-cols-2 grid-cols-1"
         >
           {imageProps.map((image, index) => (
-            <motion.div
+            <div
               key={index}
-              variants={item}
               className="w-full h-[250px] relative"
             >
               <a
@@ -84,24 +50,18 @@ const Projects = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <animated.img
-                  onMouseMove={(e) => {
-                    const { clientX: x, clientY: y } = e;
-                    return image.set({ xys: calc(x, y) });
-                  }}
-                  onMouseLeave={() => image.set({ xys: [0, 0, 1] })}
-                  style={{ transform: image.props.xys.to(trans) }}
+                <img
                   className='object-cover object-left-top w-full h-full absolute'
                   src={`/project${index + 1}.png`}
                   alt=''
                 />
               </a>
-            </motion.div>
+            </div>
           ))}
-        </motion.div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Projects;
+export default ProjectMobile;
